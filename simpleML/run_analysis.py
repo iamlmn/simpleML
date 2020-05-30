759077091c2aadf4ae8c2cabdf1804a8c5d8e849
Lakshmi Naarayanan <lakshminaarayanan.vaigaishrinivasan@cesltd.com>
2020-05-31 01:19:08 +0530

    dockerization

--- simpleML/run_analysis.py
+++ simpleML/run_analysis.py
@@ -7,14 +7,16 @@ import os
 CONFIG_FILENAME = '.config_ipynb'
 
 
-def run_notebook(argv, ipynb_filename, default_cell_timeout=600):
+def run_notebook(argv, ipynb_filename, default_cell_timeout=600, _ip = '0.0.0.0', _port = '8888'):
     with open(CONFIG_FILENAME, 'w') as f:
         f.write(' '.join(argv))
     print("Starting {}".format(ipynb_filename))
+    
+    
     os.system(
-        'jupyter nbconvert --execute {:s} --to html --TemplateExporter.exclude_input=True --ExecutePreprocessor.timeout={}'.format(
+        'jupyter nbconvert --execute {:s} --to html --TemplateExporter.exclude_input=True --ExecutePreprocessor.timeout={} --ip {} --port {} --no-browser --allow-root'.format(
             ipynb_filename,
-            default_cell_timeout))
+            default_cell_timeout), _ip, _port)
 
     return None
 
